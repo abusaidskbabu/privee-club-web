4f701388184273e4e8681c103a82fdb19e411e26
Arif Hossain <arifindex22@gmail.com>
2026-01-07 13:31:28 +0600

    Update master.blade.php

--- Modules/Admin/resources/views/layouts/master.blade.php
+++ Modules/Admin/resources/views/layouts/master.blade.php
@@ -7,28 +7,32 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Privee Club</title>
-    <script src="{{ url('assets/js/jquery-3.7.0.min.js') }}"></script>
+    <script src="{{ url('assets/js/jquery-3.7.0.min.js') }}?v={{ time() }}"></script>
     <link rel="icon" href="{{ url('assets/images/favicon.svg') }}" type="image/png">
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&amp;display=fallback">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 
-    <link rel="stylesheet" href="{{ url('assets/css/ionicons.min.css') }}">
+    <link rel="stylesheet" href="{{ url('assets/css/ionicons.min.css') }}?v={{ time() }}">
     <link rel="stylesheet"
-        href="{{ url('assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
-    <link rel="stylesheet" href="{{ url('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ url('assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+        href="{{ url('assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}?v={{ time() }}">
+    <link rel="stylesheet"
+        href="{{ url('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}?v={{ time() }}">
+    <link rel="stylesheet"
+        href="{{ url('assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}?v={{ time() }}">
     <link rel="stylesheet" href="{{ url('assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }} ">
-    <link rel="stylesheet" href="{{ url('assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+    <link rel="stylesheet"
+        href="{{ url('assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}?v={{ time() }}">
     <link rel="stylesheet" href="{{ url('assets/css/adminlte.min2167.css?v=3.2.0') }}">
-    <link rel="stylesheet" href="{{ url('assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
-    <link rel="stylesheet" href="{{ url('assets/css/custom.css') }}">
-    <link rel="stylesheet" href="{{ url('assets/plugins/summernote/summernote-bs4.min.css') }}">
+    <link rel="stylesheet"
+        href="{{ url('assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}?v={{ time() }}">
+    <link rel="stylesheet" href="{{ url('assets/css/custom.css') }}?v={{ time() }}">
+    <link rel="stylesheet" href="{{ url('assets/plugins/summernote/summernote-bs4.min.css') }}?v={{ time() }}">
     <link href="{{ url('assets/css/toastr.css') }}" rel="stylesheet">
-    <script src="{{ url('assets/js/sweetalert2.js') }}"></script>
-    <script src="{{ url('assets/js/moment.min.js') }}"></script>
-    <script src="{{ url('assets/js/daterangepicker.min.js') }}"></script>
-    <script src="{{ url('assets/js/jquery.knob.min.js') }}"></script>
+    <script src="{{ url('assets/js/sweetalert2.js') }}?v={{ time() }}"></script>
+    <script src="{{ url('assets/js/moment.min.js') }}?v={{ time() }}"></script>
+    <script src="{{ url('assets/js/daterangepicker.min.js') }}?v={{ time() }}"></script>
+    <script src="{{ url('assets/js/jquery.knob.min.js') }}?v={{ time() }}"></script>
     <link rel="stylesheet" type="text/css"
         href="https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/regular/style.css" />
 </head>
@@ -44,30 +48,33 @@
         <aside class="control-sidebar control-sidebar-dark"> </aside>
     </div>
 
-    <script src="{{ url('assets/plugins/jquery-ui/jquery-ui.min.js') }}"></script>
+    <script src="{{ url('assets/plugins/jquery-ui/jquery-ui.min.js') }}?v={{ time() }}"></script>
     <script>
         $.widget.bridge('uibutton', $.ui.button)
     </script>
-    <script src="{{ url('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ url('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}?v={{ time() }}"></script>
 
     <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>-->
     <!-- datatable scripts -->
-    <script src="{{ url('assets/plugins/datatables/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ url('assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
-    <script src="{{ url('assets/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
-    <script src="{{ url('assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+    <script src="{{ url('assets/plugins/datatables/jquery.dataTables.min.js') }}?v={{ time() }}"></script>
+    <script src="{{ url('assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}?v={{ time() }}">
+    </script>
+    <script src="{{ url('assets/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}?v={{ time() }}">
+    </script>
+    <script src="{{ url('assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}?v={{ time() }}">
+    </script>
 
     <!-- Summernote-script -->
-    <script src="{{ url('assets/plugins/summernote/summernote-bs4.min.js') }}"></script>
+    <script src="{{ url('assets/plugins/summernote/summernote-bs4.min.js') }}?v={{ time() }}"></script>
     <script src="{{ url('assets/js/adminlte2167.js?v=3.2.0') }}"></script>
-    <script src="{{ url('assets/js/demo.js') }}"></script>
-    <script src="{{ url('assets/js/pages/dashboard.js') }}"></script>
-    <script src="{{ url('assets/js/custom.js') }}"></script>
-    <script src="{{ url('assets/js/action.js') }}"></script>
+    <script src="{{ url('assets/js/demo.js') }}?v={{ time() }}"></script>
+    <script src="{{ url('assets/js/pages/dashboard.js') }}?v={{ time() }}"></script>
+    <script src="{{ url('assets/js/custom.js') }}?v={{ time() }}"></script>
+    <script src="{{ url('assets/js/action.js') }}?v={{ time() }}"></script>
 
 
-    <script src="{{ url('assets/js/toastr.min.js') }}"></script>
-    <script src="{{ url('assets/js/bootstrap.min.js') }}"></script>
+    <script src="{{ url('assets/js/toastr.min.js') }}?v={{ time() }}"></script>
+    <script src="{{ url('assets/js/bootstrap.min.js') }}?v={{ time() }}"></script>
     <script>
         @if (Session::has('message'))
             var type = "{{ Session::get('alert-type', 'info') }}";
