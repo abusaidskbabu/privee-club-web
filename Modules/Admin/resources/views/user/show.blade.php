ba66984913c2d3f6156742e2afbc99af12e466d5
Arif Hossain <arifindex22@gmail.com>
2026-01-08 15:59:20 +0600

    ssss

--- Modules/Admin/resources/views/user/show.blade.php
+++ Modules/Admin/resources/views/user/show.blade.php
@@ -435,9 +435,7 @@
         </section>
     </div>
 
-    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> --}}
+    <script src="{{ url('assets/plugins/bootstrap/js/bootstrap5.bundle.min.js') }}?v={{ time() }}"></script>
 
     <script>
         $(document).ready(function() {
